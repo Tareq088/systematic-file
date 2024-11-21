1cbe1f4fda51fae4849e03b3d3fe44e27b754579
Tareq088 <tareqrahman342@gmail.com>
2024-11-21 11:08:30 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+             // bxslider
+    $('.main-slider-area').bxSlider();
+              // Meanmenu navigation
+   
+    $("#main_menu").meanmenu({
+        meanMenuContainer:'#mobile_menu',
+        meanScreenWidth: 850,
+        meanMenuOpen: "<span></span> <span></span><span></span>",
+        
+      });
+
+});
